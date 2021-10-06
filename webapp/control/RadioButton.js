72cd66463860409e42607ffa80d60c3879857be8
성환희 <haneul9@gmail.com>
2021-10-06 16:10:21 +0900

    Radiobutton 커스텀 클래스 생성

---
+++ webapp/control/RadioButton.js
@@ -0,0 +1,30 @@
+sap.ui.define(
+	[
+		"sap/m/RadioButton", //
+	],
+	function (RadioButton) {
+		"use strict";
+
+		return RadioButton.extend("sap.ui.pub.control.RadioButton", {
+			metadata: {
+				properties: {
+					innerSize: {
+						type: "sap.ui.core.CSSSize",
+						defaultValue: "22%",
+					},
+				},
+			},
+			onAfterRendering: function () {
+				if (sap.ui.core.Control.prototype.onAfterRendering) {
+					sap.ui.core.Control.prototype.onAfterRendering.apply(this, arguments); //run the super class's method first
+				}
+
+				const sId = this.getId();
+				const oControl = document.getElementById(sId);
+
+				oControl.childNodes[0].childNodes[0].childNodes[1].setAttribute("r", this.getInnerSize());
+			},
+			renderer: {},
+		});
+	}
+);
