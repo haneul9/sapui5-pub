6d9b6741e5e186adfa31ad0d702d4cbba1f7c3ec
JHU <HPJT0897@lottechem.com>
2021-11-26 10:52:32 +0900

    대상자 팝업 데이터

--- webapp/controller/Template1Detail.controller.js
+++ webapp/controller/Template1Detail.controller.js
@@ -1,12 +1,53 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	'sap/ui/core/Fragment',
-], function (Controller, Fragment) {
+	'sap/ui/model/json/JSONModel',
+], function (Controller,
+	Fragment,
+	JSONModel) {
 	"use strict";
 
 	return Controller.extend("pub.pubsample.controller.Template1Detail", {
 
 		onDialog() {
+			const oViewModel = new JSONModel({
+				List: [],
+			});
+
+			this.getView().setModel(oViewModel);
+			var aList = [
+				{
+				Type: '딸',
+				Name: '채정안',
+				Num: '123465-2*******',
+				},
+				{
+				Type: '엄마',
+				Name: '채정이',
+				Num: '123465-2*******',
+				},
+				{
+				Type: '아들',
+				Name: '채정',
+				Num: '123465-2*******',
+				},
+				{
+				Type: '어머니',
+				Name: '채정안',
+				Num: '123465-2*******',
+				}
+				,{
+				Type: '딸',
+				Name: '채안안안',
+				Num: '1234465-2*******',
+				}
+				,{
+				Type: '딸',
+				Name: '채정안',
+				Num: '123465-2*******',
+				}
+			]
+			this.getView().getModel().setProperty('/List', aList);
 			if (!this.byId('targetSettingsDialog')) {
 				Fragment.load({
 					id: this.getView().getId(),
